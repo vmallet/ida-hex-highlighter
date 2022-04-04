1fe16973d9d402138e3f57de2d915a4b7cc0c538
Vincent Mallet <vmallet@gmail.com>
2022-04-04 12:36:49 -0700

    highlighter: use ctreeview's line highlighter
    The ctreeview's pseudocode highlighter now has everything we need
    to use it directly and remove the custom copy we had made.

--- idav_hex_highlighter.py
+++ idav_hex_highlighter.py
@@ -5,7 +5,6 @@ IDA plugin used to highlight code blocks in pseudocode windows.
 import ida_hexrays
 import ida_kernwin
 import ida_lines
-from typing import Dict, Set
 
 import LruCache
 # TODO: do not depend on idav_hex_treeview (move common dependencies out)
@@ -32,97 +31,6 @@ class HighlighterHandler(ida_kernwin.action_handler_t):
         return ida_kernwin.AST_ENABLE_FOR_WIDGET \
             if ctx.widget_type == ida_kernwin.BWN_PSEUDOCODE \
             else ida_kernwin.AST_DISABLE_FOR_WIDGET
-
-
-#TODO: hacked copy of ctreeviewer; unify and keep only one copy
-class PseudocodeHighlighter(object):
-    # TODO:
-    """TODO: An object that will highlight lines in the pseudocode that correspond to a specific citem in the ctree."""
-
-    NO_LINES = set()
-
-    class XHook(ida_kernwin.UI_Hooks):
-        def get_lines_rendering_info(self, lines_out, widget, lines_in) -> None:
-            pass
-
-    def __init__(self, entry_ea, line_map: Dict[int, Set[int]]):
-        self.entry_ea = entry_ea
-        self.line_map = line_map
-        self.ui_hooks = PseudocodeHighlighter.XHook()
-        self.ui_hooks.get_lines_rendering_info = self._maybe_highlight_lines
-        self.lines: Set[int] = set()
-        self.n = 0
-        self.current = None
-
-    def _ensure_hooks(self):
-        if not self.ui_hooks:
-            raise Exception("UI_Hooks have already been cleaned up")
-
-    def hook(self):
-        self._ensure_hooks()
-        self.ui_hooks.hook()
-
-    def unhook(self):
-        self._ensure_hooks()
-        self._set_active_lines(PseudocodeHighlighter.NO_LINES)
-        self.ui_hooks.unhook()
-        self.ui_hooks = None
-
-    def _maybe_highlight_lines(self, lines_out, widget, lines_in) -> None:
-        """Handle the get_lines_rendering_info event and decorate active lines if it's the right pseudocode widget"""
-        if ida_kernwin.get_widget_type(widget) != ida_kernwin.BWN_PSEUDOCODE or not self.lines:
-            return
-
-        vu = ida_hexrays.get_widget_vdui(widget)
-        if vu.cfunc.entry_ea != self.entry_ea:
-            return
-
-        color = ida_kernwin.CK_EXTRA14  # + self.n  # TODO: make color configurable
-        # print("color: CK_EXTRA1: {}, col: {}".format(ida_kernwin.CK_EXTRA1, color))
-        self.n = (self.n + 1) % 16
-        for line in lines_in.sections_lines[0]:  # TODO: understand what it would mean if there's more than 1 section
-            splace = ida_kernwin.place_t_as_simpleline_place_t(line.at)
-            if splace.n in self.lines:
-                entry = ida_kernwin.line_rendering_output_entry_t(line, ida_kernwin.LROEF_FULL_LINE, color)
-                lines_out.entries.push_back(entry)
-
-    def _set_active_lines(self, lines: Set[int]):
-        """Set highlighted lines and refresh view if necessary"""
-        if self.lines != lines:
-            self.lines = lines
-            ida_kernwin.refresh_idaview_anyway()
-
-    def set_current(self, ci: ida_hexrays.citem_t, full_block=False):
-        """Set current ctree item for which lines should be highlighted"""
-        self.current = ci
-        if not ci:
-            lines = PseudocodeHighlighter.NO_LINES
-        else:
-            if full_block:
-                lines = self._collect_lines(ci)
-            else:
-                lines = self.line_map.get(ci.obj_id, PseudocodeHighlighter.NO_LINES)
-
-        self._set_active_lines(lines)
-
-    def _collect_lines(self, citem):
-        class lc(ida_hexrays.ctree_visitor_t):
-            def __init__(self, map):
-                ida_hexrays.ctree_visitor_t.__init__(self, ida_hexrays.CV_FAST)
-                self.lines = set()
-                self.line_map = map
-
-            def visit_insn(self, insn) -> int:
-                self.lines.update(self.line_map.get(insn.obj_id, PseudocodeHighlighter.NO_LINES))
-                return 0
-
-            def visit_expr(self, expr) -> int:
-                self.lines.update(self.line_map.get(expr.obj_id, PseudocodeHighlighter.NO_LINES))
-                return 0
-
-        lcx = lc(self. line_map)
-        lcx.apply_to(citem, None)
-        return lcx.lines
 
 
 class BlockHighlighter(object):
@@ -194,11 +102,12 @@ class BlockHighlighter(object):
 
     def _set_current(self, cfunc, citem):
         high = self.highlighters.get(cfunc.entry_ea, lambda: self._create_highlighter(cfunc))
-        high.set_current(citem, full_block=True)
+        high.set_current(citem, include_children=True)
         ida_kernwin.refresh_idaview_anyway()
 
     def _create_highlighter(self, cfunc):
-        h = PseudocodeHighlighter(cfunc.entry_ea, idav_hex_treeview.map_citems_to_lines(cfunc))
+        h = idav_hex_treeview.PseudocodeHighlighter(
+            cfunc.entry_ea, idav_hex_treeview.map_citems_to_lines(cfunc), ida_kernwin.CK_EXTRA14)
         h.hook()
         return h
 
